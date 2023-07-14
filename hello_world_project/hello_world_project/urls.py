5af4e35a7d7714f4d6a7d7c34f733e3a0ace6045
suuu0122 <suuu0122lab@gmail.com>
2023-07-14 10:54:02 +0900

    set url settings

--- hello_world_project/hello_world_project/urls.py
+++ hello_world_project/hello_world_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("hello_world_app.urls")),
 ]
