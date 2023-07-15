bb08f464fb24a498fa05d1712815dd99aebca4c3
suuu0122 <suuu0122lab@gmail.com>
2023-07-15 09:34:55 +0900

    add view to display using class

--- hello_world_project/hello_world_app/urls.py
+++ hello_world_project/hello_world_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 
 
+app_name = "hello_world_app"
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('hello_world_function', views.index, name="fucntion_index"),
+    path('hello_world_class', views.IndexView.as_view(), name="class_index"),
 ]
--- hello_world_project/hello_world_app/views.py
+++ hello_world_project/hello_world_app/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView
 
 
 def index(request):
     response = HttpResponse("<h1>Hello World!</h1>")
     return response
+
+
+
+class IndexView(TemplateView):
+    template_name = "hello_world_app/index.html"
